e085c6449b7d7a0464c7fecbcb5534bcff778dc7
amirmohammadi <fskyfall47@gmail.com>
2022-05-07 00:13:53 +0430

    jwt authentication in .net 5

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using jwt.IdentityAuth;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace jwt.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+    }
+}
---
+++ Moldes/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace jwt.Moldes
+{
+    public class UserRoles
+    {
+        public static string Admin = "Admin";
+        public static string User = "User";
+    }
+}
